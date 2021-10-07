268fb448c7defc1ee802bb7b1c3738dab3576bb3
minaatta-IOS <minamagdyfci@gmail.com>
2021-10-08 00:11:24 +0200

    init

---
+++ shorkCut_challange/shorkCut_challange/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  shorkCut_challange
+//
+//  Created by Mina on 10/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
